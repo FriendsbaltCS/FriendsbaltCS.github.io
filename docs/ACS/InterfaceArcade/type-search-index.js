c12a60119d8477347dcf6ddb170bde03718fea2e
jahAtFriends <jhammer@friendsbalt.org>
2021-09-23 21:52:07 -0400

    Add docs for Interface Arcade

---
+++ docs/ACS/InterfaceArcade/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"org.friendsbalt.cs.adv.InterfaceArcade","l":"TwoPlayerGame.Player"},{"p":"org.friendsbalt.cs.adv.InterfaceArcade","l":"TwoPlayerGame"}];updateSearchResults();
